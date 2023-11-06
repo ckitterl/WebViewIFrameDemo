e4bfe007b15fdf828ab20de8e0840199da7e2549
LarryCai <cmukun@gmail.com>
2023-11-07 00:45:59 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+## 这是一个简单的webview对象注入的demo，主要验证2个功能
+1. asset资源的访问
+2. iframe访问android注入对象的情况
---
+++ app/src/main/java/com/test/webview/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.test.webview
+
+import android.net.Uri
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
+import com.test.webview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val webView = binding.contentMain.webView
+
+        /**
+         * 这里设置路径访问规则，让WebView可以访问in-app的内容
+         * https://developer.android.com/develop/ui/views/layout/webapps/load-local-content
+         */
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
+            .build()
+        webView.webViewClient = LocalContentWebViewClient(assetLoader)
+
+        // 注入对象
+        webView.settings.javaScriptEnabled = true // 这样网页就可加载JavaScript了
+        webView.webChromeClient = object: WebChromeClient() {}
+//        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
+        webView.addJavascriptInterface(this, "android")
+    }
+
+    @JavascriptInterface fun call(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
+        @RequiresApi(21)
+        override fun shouldInterceptRequest(
+            view: WebView,
+            request: WebResourceRequest
+        ): WebResourceResponse? {
+            return assetLoader.shouldInterceptRequest(request.url)
+        }
+
+        // To support API < 21.
+        override fun shouldInterceptRequest(
+            view: WebView,
+            url: String
+        ): WebResourceResponse? {
+            return assetLoader.shouldInterceptRequest(Uri.parse(url))
+        }
+    }
+}
